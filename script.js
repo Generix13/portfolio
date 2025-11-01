5c9f759d5c2222b843c6dc1d5eb48814b0f058d5
Erick Hernandez <108959571+Generix13@users.noreply.github.com>
2025-11-01 00:23:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+﻿window.onload = function () {
+    console.log("Welcome to Erick's portfolio!");
+};
+
+const contactForm = document.getElementById('contact-form');
+contactForm.addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent page reload
+    const name = this.name.value;
+    const email = this.email.value;
+    const message = this.message.value;
+    console.log(`New message from ${name} (${email}): ${message}`);
+    alert('Thank you for your message!');
+    this.reset();
+});
